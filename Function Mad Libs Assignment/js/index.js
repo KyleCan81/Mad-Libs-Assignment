ab380dc23fedb66b369e5a009423280cb82930ab
KyleCan81 <kylecan8112@gmail.com>
2024-10-21 10:38:36 -0400

    Add files via upload

---
+++ Function Mad Libs Assignment/js/index.js
@@ -0,0 +1,14 @@
+const typeOfBirdInput = document.getElementById('typeOfBird');
+const typeOfBird = typeOfBirdInput.value;
+
+const typeOfColorInput = document.getElementById('typeOfColor');
+const typeOfColor = typeOfColorInput.value;
+
+const typeOfCandyInput = document.getElementById('typeOfCandy');
+const typeOfCandy = typeOfCandyInput.value;
+
+const typeOfAnimalInput = document.getElementById('typeOfAnimal');
+const typeOfAnimal = typeOfAnimalInput.value;
+
+const typeOfMonsterInput = document.getElementById('typeOfMonster');
+const typeOfMonster = typeOfMonsterInput.value;
